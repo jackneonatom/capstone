145b570de51f98eeafb6c9a609acfd8702f29970
Nathanm <122718848+jackneonatom@users.noreply.github.com>
2025-01-23 12:35:15 -0500

    Update notes.sh

--- notes/notes.sh
+++ notes/notes.sh
@@ -1,15 +1,21 @@
-ssh -i id_rsa.pub ubuntu@164.152.104.200
+ssh -i id_rsa.pub ubuntu@164.152.104.200  #ssh into public server
 
-
-sudo apt update
+#update package list
+sudo apt update 
+#update system
 sudo apt upgrade
+#install nginx and view status
 sudo apt install nginx
 systemctl status nginx
+#install text editor vim
 sudo apt install vim
+#edit nginx config file
 sudo vim /etc/nginx/sites-available/default
 cd /var/www/html
 ls
+#change permissions so file can be added
 sudo chmod 777 /var/www/html/
+#install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 
 sh get-docker.sh
@@ -20,30 +26,42 @@ docker run --restart=always -d -p 27017:27017 --name mongodb mongo
 sudo docker pull mongo:3.6
 sudo docker run -d --name my-mongo -p 27017:27017 mongo:3.6
 # ---------------------------------------------------------------------------
+#setting up fastapi 
 pip install fastAPI uvicorn motor datetime
+#setting up raspberry pi gpio
 
 pip3 install RPi.GPIO
-
+#check docker containers
 sudo docker ps
+#########
 cat default
 cd ~
 sudo raspi-config #then change the hostname to whatever you want itsudo -H ./install
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 logout #have to logout to reload node js when installed
-
+# install node.js and setup contant running of python file
 nvm install 20
 npm install pm2 -g
 pm2 start app.py
+# check pm2 logs
 pm2 monit
+#python 3 installed version
 python3 --version
+#stop constant runnug app
 pm2 stop app.py
+# install python packages system wide on linux system
 sudo apt install python3-fastapi
 sudo apt install python3-motor
+#run program on linux system
 python3 app.py
+#check status of constantly runnig=ng apps
 pm2 status
+#delete constantly runnign program
 pm2 delete app
+#start it back up choosing interpreter 
 pm2 start app.py --interpreter python3
+#check pm2 logs
 pm2 logs app
 pm2 startup  #only need to run once
 # startup should then return a path and a command that you need to run to actually do the startup
@@ -67,8 +85,9 @@ show dbs
 sudo apt install vlc -y
 vlc v4l2:///dev/video0
 
+#install postgres database in docker container
 
 docker run --name postgresdb -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin123 -e POSTGRES_DB=vehiclecounter -p 5432:5432 -d postgres:latest -e TZ=America/Bogota
 
 # frequency over a time period or different periods and then compare data
-have drop down menu on each icon that shows different frequecnies in different time periods
+#have drop down menu on each icon that shows different frequecnies in different time periods
