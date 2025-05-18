1f82dd0f0dac2dd4ad8a22142e66ced2c107c191
Nathanm <122718848+jackneonatom@users.noreply.github.com>
2025-05-17 19:00:54 -0500

    Update capstone.js

--- webpage/js/capstone.js
+++ webpage/js/capstone.js
@@ -1,6 +1,6 @@
 async function fetchData() {
     try {
-        const response = await fetch('http://ec2-13-59-11-204.us-east-2.compute.amazonaws.com:8002/count', {
+        const response = await fetch('http://ec2-13-59-11-204.us-east-2.compute.amazonaws.com:80/count', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
